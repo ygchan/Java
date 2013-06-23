6f8c344321033ce13a0c2d3c851777e12b8dc39d
George <ygchan89@gmail.com>
2013-06-23 19:53:19 -0400

    Constant should always be in uppercase

--- ComputeRectangleArea.java
+++ ComputeRectangleArea.java
@@ -5,7 +5,8 @@ class ComputeRectangleArea {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		final double width = 10; // Declare a constant 
+		// Declare a constant, and they should always be in upper case
+		final double WIDTH = 10; 
 		
 		// Obtain the user input for the length and width
 		System.out.print("Enter a number for the length: ");
@@ -14,10 +15,10 @@ class ComputeRectangleArea {
 		// double width = input.nextDouble();
 		
 		// Compute the area of a rectangle
-		double areaOfRectangle = length * width;
+		double areaOfRectangle = length * WIDTH;
 		
 		// Display the result of the area calculation
-		System.out.print("The area of a " + length + " x " + width 
+		System.out.print("The area of a " + length + " x " + WIDTH 
 				+ "rectangle is " + areaOfRectangle);
 	}
 }

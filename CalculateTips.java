b52a92d438b25ca1f448d5ae8c484bebe0cf82e2
George <ygchan89@gmail.com>
2013-06-24 21:52:32 -0400

    Attempt to fix the loss of accuracy

--- CalculateTips.java
+++ CalculateTips.java
@@ -5,10 +5,14 @@ import java.util.Scanner;
 class CalculateTips {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		
+		// Prompt the user for input
+		System.out.println("Note: Please enter your number as 1156" 
+				+ " instead of 11.56");
 		System.out.print("Enter the cost of your dinner: ");
 		
-		double costOfDinner = input.nextDouble();
-		double tip = costOfDinner * 0.15;
+		int costOfDinner = input.nextInt();
+		double tip = costOfDinner * 0.15 / 100;
 		
 		System.out.print("A 15% tip is: " + (int)(tip * 100 / 100.0));
 	}

985519603c648405b8137b57a85250df71bc259c
George <ygchan89@gmail.com>
2013-06-24 21:38:34 -0400

    Display welcome in Chinese

---
+++ DisplayUnicode.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class DisplayUnicode {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "\u6B22\u8FCE", "\u6B22\u8FCE", 
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+}

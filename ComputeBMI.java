584d6f674b3bc6e9665d09a8c8b55da3b9297c30
George <ygchan89@gmail.com>
2013-06-26 21:35:38 -0400

    Add a file

---
+++ ComputeBMI.java
@@ -0,0 +1,28 @@
+// A simple program that compute the Body Mass Index
+
+import java.util.Scanner;
+
+class ComputingBMI {
+	public static void main(String[] args) {
+		// Prompt user to input the weight
+		System.out.print("Enter weight in pounds: ");
+		Scanner input = new Scanner(System.in);
+		double weightInPounds = input.nextDouble();
+		
+		// Convert the weight from pounds to kilogram
+		double weightInKilograms = weightInPounds * 0.4535;
+		
+		// Prompt user to input the height
+		System.out.print("Enter height in inches: ");
+		double heightInInches = input.nextDouble();
+		
+		// Convert the height from inches to meters
+		double heightInMeters = heightInInches * 0.0254;
+		
+		// Compute the Body Mass Index
+		// Formula: (weight) kg / (height) m^2
+		double BMI = weightInKilograms / Math.pow(heightInMeters, 2.0);
+			
+		System.out.println("BMI is " + BMI);
+	}
+}

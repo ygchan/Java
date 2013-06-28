5c02467add09a93a1fc149871cac252f85e826de
George <ygchan89@gmail.com>
2013-06-27 21:56:16 -0400

    Simple question

--- PrintingTable.java
+++ PrintingTable.java
@@ -1,5 +1,11 @@
 class PrintingTable {
 	public static void main(String[] args) {
 		System.out.println("a \tb \tpow(a, b)" );
+		System.out.println("1 \t2 \t" + Math.pow(1, 2));
+		System.out.println("2 \t3 \t" + Math.pow(2, 3));
+		System.out.println("3 \t4 \t" + Math.pow(3, 4));
+		System.out.println("4 \t5 \t" + Math.pow(4, 5));
+		System.out.println("5 \t6 \t" + Math.pow(5, 6));
+		
 	}
 }

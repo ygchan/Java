03ac0ae190ff733534bc9f1a9d1e98595b13c650
George <ygchan89@gmail.com>
2013-06-27 22:16:12 -0400

    Learn that .nextDouble() works similar to cin >> x >> y

---
+++ DistanceOfTwoPoints.java
@@ -0,0 +1,16 @@
+// Compute the distance of two points
+
+import java.util.Scanner;
+
+class DistanceOfTwoPoints{
+	public static void main(String[] args) {
+		// Prompt the user to enter (x1, y1)
+		Scanner input = new Scanner (System.in);
+		System.out.print("Enter x1 and y1: ");
+		double x1 = input.nextDouble();
+		double y1 = input.nextDouble();
+		
+		System.out.print(x1);
+		System.out.print(y1);
+	}
+}

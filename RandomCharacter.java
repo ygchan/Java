aac6272ffe26dcbc16ebd62ab1d0140a604e77d8
George <ygchan89@gmail.com>
2013-06-27 22:02:13 -0400

    Add a file

---
+++ RandomCharacter.java
@@ -0,0 +1,14 @@
+// A program that displays a random uppercase letter using 
+// the System.CurrentTimeMillis() method
+
+class RandomCharacter{
+	public static void main(String[] args){
+		// A sudo random value that is based on the current time
+		long randomSeed = System.currentTimeMillis();
+		
+		randomSeed = randomSeed / 1000 % 60 ;
+		char ch = (char)randomSeed;
+		
+		System.out.println(ch);
+	}
+}

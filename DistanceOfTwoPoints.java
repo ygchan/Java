98859ce46ef841a887cb80dbdaa0f3fe7a1b519d
George <ygchan89@gmail.com>
2013-06-28 21:33:15 -0400

    Competed the program

--- DistanceOfTwoPoints.java
+++ DistanceOfTwoPoints.java
@@ -4,13 +4,23 @@ import java.util.Scanner;
 
 class DistanceOfTwoPoints{
 	public static void main(String[] args) {
-		// Prompt the user to enter (x1, y1)
 		Scanner input = new Scanner (System.in);
+		
+		// Prompt the user to enter (x1, y1)
 		System.out.print("Enter x1 and y1: ");
 		double x1 = input.nextDouble();
 		double y1 = input.nextDouble();
 		
-		System.out.print(x1);
-		System.out.print(y1);
+		// Prompt the user to enter (x2, y2)
+		System.out.print("Enter x2 and y2: ");
+		double x2 = input.nextDouble();
+		double y2 = input.nextDouble();
+		
+		// Compute the distance between the two point
+		double distance = Math.pow(Math.pow(x2-x1, 2) 
+				+ Math.pow(y2-y1, 2), 0.5);
+		
+		// Display the answer
+		System.out.print("The distance of two points is " + distance);
 	}
 }

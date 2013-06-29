ba351785f2608a51ca7901da524e22ccbf6508e1
George <ygchan89@gmail.com>
2013-06-29 08:42:57 -0400

    Learn a shorter way to determine the boolean value

--- IsEven.java
+++ IsEven.java
@@ -2,12 +2,8 @@ class IsEven {
 	public static void main(String[] args) {
 		// A simple random number
 		int number = (int)(System.currentTimeMillis() % 10);
-		boolean even;
-		
-		if (number % 2 == 0)
-			even = true;
-		else
-			even = false;
+		// A shorter method to get the boolean value of even
+		boolean even = number % 2 == 0;
 		
 		System.out.println("The number " + number + " is even? " + even);
 	}

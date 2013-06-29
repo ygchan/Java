fe2ca80ecade6866567ad73995b1a3a23a371218
George <ygchan89@gmail.com>
2013-06-29 07:55:48 -0400

    Add a file

---
+++ GuessBirthday.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class GuessBirthday {
+	public static void main(String[] args) {
+		String set1 =
+				"1 3 5 7 9 11 13 15\n" +
+				"17 19 21 23 25 27 29 31";
+		
+		String set2 = 
+				"2 3 6 7 10 11 14 15\n" +
+				"18 19 22 23 26 27 30 31";
+		
+		String set3 =
+				"4 5 6 7 12 13 14 15\n" +
+				"20 21 22 23 28 29 30 31";
+		
+		String set4 = 
+				"8 9 10 11 12 13 14 15\n" +
+				"24 25 26 27 28 29 30 31\n";
+		
+		String set5 = 
+				"16 17 18 19 20 21 22 23\n" +
+				"24 25 26 27 28 29 30 31";
+		
+		int day = 0;
+		
+		// Create a Scanner
+		Scanner input = new Scanner(System.in);
+		
+	}
+}

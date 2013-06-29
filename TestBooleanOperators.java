455393d4d9886503092fd5f5d7d7765d86b60231
George <ygchan89@gmail.com>
2013-06-29 13:05:18 -0400

    Add a file

---
+++ TestBooleanOperators.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TestBooleanOperators {
+	public static void main(String[] args) {
+		// Create a Scanner
+		Scanner input = new Scanner(System.in);
+		
+		// Receive an input
+		System.out.print("Enter an integer: ");
+		int number = input.nextInt();
+		
+		System.out.println("Is " + number + 
+				"\n\tdivisible by 2 and 3? " +
+				(number % 2 == 0 && number % 3 == 0)
+				+ "\n\tdivisible by 2 or 3? " +
+				(number % 2 == 0 || number % 3 == 0) +
+				"\n\tdivisible by 2 or 3, but not both? " 
+				+ (number % 2 == 0 ^ number % 3 == 0));
+	}
+}

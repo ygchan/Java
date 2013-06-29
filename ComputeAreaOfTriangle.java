7bb5ef439bbc711d0c8d484c7fe39ff49dd97b1a
George <ygchan89@gmail.com>
2013-06-28 21:51:30 -0400

    Not as bad as I thought

--- ComputeAreaOfTriangle.java
+++ ComputeAreaOfTriangle.java
@@ -23,6 +23,18 @@ class ComputeAreaOfTriangle {
 		y3 = input.nextDouble();
 		
 		// Compute the area 
-		double s;
+		double side1 = Math.pow(
+				(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2)), 0.5);
+		double side2 = Math.pow(
+				(Math.pow(x3-x2, 2) + Math.pow(y3-y2, 2)), 0.5);
+		double side3 = Math.pow(
+				(Math.pow(x1-x3, 2) + Math.pow(y1-y3, 2)), 0.5);
+		double s = (side1 + side2 + side3) / 2;
+		double area = Math.pow(
+				s * (s - side1) * (s - side2) * (s - side3), 0.5);
+		
+		// Display the result
+		System.out.println("The area of a triangle is " + area);
+		
 	}
 }

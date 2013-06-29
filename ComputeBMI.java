63783d5244ad4e953634616c864cfab7e39adebf
George <ygchan89@gmail.com>
2013-06-29 17:30:50 -0400

    Fixes #3
    Add the feature to enter feet and inch seperately, and implemet a GUI
    for entering information.

--- ComputeBMI.java
+++ ComputeBMI.java
@@ -1,17 +1,23 @@
 // A simple program that compute the Body Mass Index
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 class ComputeBMI {
 	public static void main(String[] args) {
 		// Prompt user to input the weight
-		System.out.print("Enter weight in pounds: ");
-		Scanner input = new Scanner(System.in);
-		double weight = input.nextDouble();
+		String weightString = JOptionPane.showInputDialog(
+				"Enter weight in pounds:");
+		double weight = Double.parseDouble(weightString);
 		
 		// Prompt user to input the height
-		System.out.print("Enter height in inches: ");
-		double height = input.nextDouble();
+		String feetString = JOptionPane.showInputDialog(
+				"Enter height (feet part): ");
+		String inchString = JOptionPane.showInputDialog(
+				"Enter height (inch part): ");
+		
+		double feet = Double.parseDouble(feetString);
+		double inch = Double.parseDouble(inchString);
+		double height = feet * 12 + inch;
 		
 		final double KILOGRAMS_PER_POUND = 0.45359237;
 		final double METERS_PER_INCH = 0.0254;
@@ -21,19 +27,23 @@ class ComputeBMI {
 		double heightInMeters = height * METERS_PER_INCH;
 		double bmi = weightInKilgrams / Math.pow(heightInMeters, 2);
 		
+		String result;
+		
 		// Display result
 		System.out.printf("Your BMI is %5.2f\n", bmi);
 		if (bmi < 16)
-			System.out.println("You are seriously underweight");
+			result = "You are seriously underweight";
 		else if (bmi < 18)
-			System.out.println("You are underweight");
+			result = "You are underweight";
 		else if (bmi < 24)
-			System.out.println("You are normal weight");
+			result = "You are normal weight";
 		else if (bmi < 29)
-			System.out.println("You are over weight");
+			result = "You are over weight";
 		else if (bmi < 35)
-			System.out.println("You are seriously overweight");
+			result = "You are seriously overweight";
 		else
-			System.out.println("You are gravely overweight");
+			result = "You are gravely overweight";
+		
+		JOptionPane.showMessageDialog(null, result);
 	}
 }

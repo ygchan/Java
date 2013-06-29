dd9fbb6012137bc7978015e06a42698d7cdf5d5f
George <ygchan89@gmail.com>
2013-06-29 07:41:10 -0400

    Update to the random number generate
    Learned how to generate number with System.currentTimeMillis()

--- AdditionQuiz.java
+++ AdditionQuiz.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class AdditionQuiz {
 	public static void main(String[] args) {
 		int number1 = (int)(System.currentTimeMillis() % 10);
-		int number2 = (int)(System.currentTimeMillis() % 7);
+		int number2 = (int)(System.currentTimeMillis() * 7 / 10);
 		
 		// Create a Scanner
 		Scanner input = new Scanner(System.in);

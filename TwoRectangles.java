a9fcbabd042d828f90488441c3a9b8440d6bc0d0
George <ygchan89@gmail.com>
2013-06-30 13:32:26 -0400

    Test isOverLap logic

--- TwoRectangles.java
+++ TwoRectangles.java
@@ -6,6 +6,7 @@ public class TwoRectangles {
 		// Prompt user to enter the coordinates of two rectangles
 		
 		// Create test case
+		/* Test case for Inside
 		double r1cx = 2.5; // rectangle 1's center x coordinate
 		double r1cy = 4; // rectangle 1's center y coordinate
 		double r1width = 2.5; // rectangle 1's width
@@ -15,6 +16,18 @@ public class TwoRectangles {
 		double r2cy = 5;
 		double r2width = 0.5;
 		double r2height = 3;
+		*/
+		
+		// Test case for Overlap
+		double r1cx = 1; // rectangle 1's center x coordinate
+		double r1cy = 2; // rectangle 1's center y coordinate
+		double r1width = 3; // rectangle 1's width
+		double r1height = 5.5; // rectangle 1's height
+		
+		double r2cx = 3;
+		double r2cy = 4;
+		double r2width = 4.5;
+		double r2height = 5;
 		
 		// Compute the 4 corners's coordinates for the rectangle
 		double r1x1 = r1cx - (r1width / 2.0);
@@ -41,12 +54,15 @@ public class TwoRectangles {
 			(r1y1 >= r2y1) && (r1y2 <= r2y2));
 		
 		// System.out.println(
-		//	 "Is rectangle 2 inside of rectangle 1?" + isInside);
+		//	 "Is rectangle 2 inside of rectangle 1? " + isInside);
 		
 		// Determine if it is overlap
-		boolean isCoverLap = 
+		boolean isOverLap = 
 				((r1x1 <= r2x1) || (r1x2 >= r2x2) || 
 				(r1y1 >= r2y1) || (r1y2 <= r2y2));
+		
+		System.out.println(
+			"Is rectangle 2 overlapping rectangle 1? " + isOverLap);
 		
 		// Determine if it is not overlap
 		

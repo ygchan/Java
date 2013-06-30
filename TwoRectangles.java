278230507ca1dbbf476592e67803e10508d8f3fa
George <ygchan89@gmail.com>
2013-06-30 13:01:57 -0400

    Create test case

--- TwoRectangles.java
+++ TwoRectangles.java
@@ -5,6 +5,18 @@ public class TwoRectangles {
 	public static void main(String[] args) {
 		// Prompt user to enter the coordinates of two rectangles
 		
+		// Create test case
+		double r1cx = 2.5; // rectangle 1's center x coordinate
+		double r1cy = 4; // rectangle 1's center y coordinate
+		double r1width = 2.5; // rectangle 1's width
+		double r1height = 43; // rectangle 1's height
+		
+		double r2cx = 1.5;
+		double r2cy = 5;
+		double r2width = 0.5;
+		double r2height = 43;
+	
+		
 		// Determine if it is inside
 		
 		// Determine if it is overlap

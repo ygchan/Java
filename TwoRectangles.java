60d6d55bc8e21a287c3f67539a7cb0846ddb939e
George <ygchan89@gmail.com>
2013-06-30 13:14:13 -0400

    Implement the isInside logic

--- TwoRectangles.java
+++ TwoRectangles.java
@@ -36,7 +36,9 @@ public class TwoRectangles {
 			r2x1, r2y1, r2x2, r2y2);
 		
 		// Determine if it is inside
-		
+		boolean isInside = 
+			((r1x1 >= r2x1) && (r1x2 >= r2x2) && 
+			(r1y1 >= r2y1) && (r1y2 >= r2y2));
 		
 		// Determine if it is overlap
 		

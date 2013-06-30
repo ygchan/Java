b40211a5078967d626aa0fa19d762a7b2db0621c
George <ygchan89@gmail.com>
2013-06-30 16:11:39 -0400

    Print Hello Java 5 times

--- HelloJava.java
+++ HelloJava.java
@@ -2,6 +2,10 @@
 
 public class HelloJava {
 	public static void main(String[] args) {
-		
+		System.out.println("Hello Java!");
+		System.out.println("Hello Java!");
+		System.out.println("Hello Java!");
+		System.out.println("Hello Java!");
+		System.out.println("Hello Java!");
 	}
 }

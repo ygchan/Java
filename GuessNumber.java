5a36c981bf21f68bf3ec1dca6d3c3d33b4243a1c
George <ygchan89@gmail.com>
2013-06-30 16:44:30 -0400

    Implement a loop allow user to guess it many times

--- GuessNumber.java
+++ GuessNumber.java
@@ -7,18 +7,24 @@ public class GuessNumber {
 		// Generate a random number to be guessed
 		// This will generate a random number between [0, 101)
 		int number = (int)(Math.random() * 101);
+		// System.out.println("The number is " + number);
 		
-		// Prompt user to enter a guess
+		// Create a scanner
 		Scanner input = new Scanner(System.in);
-		System.out.println("Guess a magic number between 0 and 100");
-		int guess = input.nextInt();
+		int guess = -1; 
 		
-		// Determine if the guess is correct
-		if (guess == number)
-			System.out.print("Yes, the number is " + number);
-		else if (guess > number)
-			System.out.println("Your guess is too high");
-		else
-			System.out.println("Your guess is too low");
+		// While the user did not guess it right
+		while (guess != number) {
+			System.out.println("Guess a magic number between 0 and 100");
+			guess = input.nextInt();
+			
+			if (guess == number)
+				System.out.print("Yes, the number is " + number);
+			else if (guess > number)
+				System.out.println("Your guess is too high");
+			else
+				System.out.println("Your guess is too low");
+		}
+		
 	}
 }

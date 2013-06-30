7cf2ccd1629e3d60e74a7e3097c7f668a227147c
George <ygchan89@gmail.com>
2013-06-30 07:46:08 -0400

    Add the design

---
+++ GameRockPaperScissor.java
@@ -0,0 +1,17 @@
+// A game that plays rock paper scissor with user
+
+// Import the javax.swing
+
+public class GameRockPaperScissor {
+	public static void main(String[] args) {
+		// Prompt the user to input the optino they choose
+		
+		// Generate a random number
+		// Scissor (0),  rock (1), paper (2)
+		
+		// Determine who win
+		// There are 3 possibility, win, lose, or tie
+		
+		// Display the result on the screen
+	}
+}

8f10b12f018ca5c10dcacdfbafdc566452830c11
George <ygchan89@gmail.com>
2013-06-30 13:45:35 -0400

    Remove unnecessary part

--- TwoRectangles.java
+++ TwoRectangles.java
@@ -42,15 +42,12 @@ public class TwoRectangles {
 		boolean isOverLap = ((r1x1 <= r2x1) || 
 				(r1x2 >= r2x2) || (r1y1 >= r2y1) || (r1y2 <= r2y2));
 
-		// Determine if it is not overlap
-		boolean isNotOverLap = !(isOverLap);
-
 		// Display result
 		if (isInside) 
 			System.out.print("r2 is inside r1");
 		else if (isOverLap)
 			System.out.print("r2 overlap r1");
-		else if (isNotOverLap)
-			System.out.print("r2 does not overlap r1");
+			else
+				System.out.print("r2 does not overlap r1");
 	}
 }

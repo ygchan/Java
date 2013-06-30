bac440e432655ff75bf6c947328bd394cca5fb6c
George <ygchan89@gmail.com>
2013-06-30 08:55:16 -0400

    Add the design

---
+++ DayOfTheWeek.java
@@ -0,0 +1,17 @@
+// Implement the Zeller's congruence algorithm.
+// Formula: h = (q + floor(26(m+1)/10 + k + floor(k/4) + floor(j/4) + 5j) % 7
+// To calculate the day of the week
+
+public class DayOfTheWeek {
+	public static void main(String[] args) {
+		// Prompt the user to enter a year, month and a day
+		
+		// Check if the month is January or February
+		// If the month is January and February, convert to 13, and 14,
+		// and year has to -1. (Go to previous year).
+		
+		// Compute the answer
+		
+		//Display the name of the day of the week
+	}
+}

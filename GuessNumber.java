2b80f584f3f8d370506ed663d837a317ec07d3de
George <ygchan89@gmail.com>
2013-06-30 16:38:37 -0400

    Add a design

---
+++ GuessNumber.java
@@ -0,0 +1,13 @@
+// Guess number one time
+
+import java.util.Scanner;
+
+public class GuessNumber {
+	public static void main(String[] args) {
+		// Generate a random number to be guessed
+		
+		// Prompt user to enter a guess
+		
+		// Determine if the guess is correct
+	}
+}

e2f5ca6de9491b8fc72b9933d0e09d792ace1236
George <ygchan89@gmail.com>
2013-06-30 11:41:45 -0400

    Add the design

---
+++ PointInTriangle.java
@@ -0,0 +1,17 @@
+// A program that prompt user for a point and check if it is in the triangle
+
+// Import the Util
+
+public class PointInTriangle {
+	public static void main(String[] args) {
+		// Create a Scanner
+		
+		// Prompt user for input for point
+		
+		// Compute the Slope the the triangle
+		
+		// Determine if the point is in thez triangle
+		
+		// Display the result
+	}
+}

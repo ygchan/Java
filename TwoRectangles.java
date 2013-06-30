b8e6cdde80cae57c61a13eac4c24957ffb918583
George <ygchan89@gmail.com>
2013-06-30 13:35:53 -0400

    Implement the isNotOverLap logic

--- TwoRectangles.java
+++ TwoRectangles.java
@@ -6,7 +6,9 @@ public class TwoRectangles {
 		// Prompt user to enter the coordinates of two rectangles
 		
 		// Create test case
-		/* Test case for Inside
+		
+		// Test case for Inside
+		/*
 		double r1cx = 2.5; // rectangle 1's center x coordinate
 		double r1cy = 4; // rectangle 1's center y coordinate
 		double r1width = 2.5; // rectangle 1's width
@@ -19,6 +21,7 @@ public class TwoRectangles {
 		*/
 		
 		// Test case for Overlap
+		/*
 		double r1cx = 1; // rectangle 1's center x coordinate
 		double r1cy = 2; // rectangle 1's center y coordinate
 		double r1width = 3; // rectangle 1's width
@@ -28,6 +31,18 @@ public class TwoRectangles {
 		double r2cy = 4;
 		double r2width = 4.5;
 		double r2height = 5;
+		*/
+		
+		// Test case for Not Overlap
+		double r1cx = 1; // rectangle 1's center x coordinate
+		double r1cy = 2; // rectangle 1's center y coordinate
+		double r1width = 3; // rectangle 1's width
+		double r1height = 3; // rectangle 1's height
+		
+		double r2cx = 40;
+		double r2cy = 45;
+		double r2width = 3;
+		double r2height = 2;
 		
 		// Compute the 4 corners's coordinates for the rectangle
 		double r1x1 = r1cx - (r1width / 2.0);
@@ -53,18 +68,21 @@ public class TwoRectangles {
 			((r1x1 <= r2x1) && (r1x2 >= r2x2) && 
 			(r1y1 >= r2y1) && (r1y2 <= r2y2));
 		
-		// System.out.println(
-		//	 "Is rectangle 2 inside of rectangle 1? " + isInside);
+		/*System.out.println(
+		 * "Is rectangle 2 inside of rectangle 1? " + isInside);*/
 		
 		// Determine if it is overlap
 		boolean isOverLap = 
 				((r1x1 <= r2x1) || (r1x2 >= r2x2) || 
 				(r1y1 >= r2y1) || (r1y2 <= r2y2));
 		
-		System.out.println(
-			"Is rectangle 2 overlapping rectangle 1? " + isOverLap);
+		/*System.out.println(
+		 * "Is rectangle 2 overlapping rectangle 1? " + isOverLap);*/
 		
 		// Determine if it is not overlap
+		boolean isNotOverLap = !(isOverLap);
+		System.out.println(
+		  "Is rectangle 2 overlapping rectangle 1? " + isNotOverLap);
 		
 		// Display result
 	}

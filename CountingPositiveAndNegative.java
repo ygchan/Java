18c4b9a5bd336afdd158796d4196fea4bcbe93a6
ygchan89 <ygchan89@gmail.com>
2013-07-04 15:35:03 -0400

    Add a solution design

---
+++ CountingPositiveAndNegative.java
@@ -0,0 +1,18 @@
+// Write a program that reads an unspecified number of integers, determine
+// how many positive and negative values have been read, and compute the
+// total and average of the input value (not counting zeros).
+
+public class CountingPositiveAndNegative {
+	public static void main(String[] args) {
+		// Display the instruction
+		
+		// Prompt the user to input a list of number
+		
+		// While the number is not 0
+			// Count the positive
+			// Count the negative
+			// Count the total
+		
+		// Compute the total and the average
+	}
+}

773b934e983629f515c2cfd68f17a05708ba7389
ygchan89 <ygchan89@gmail.com>
2013-07-04 09:14:17 -0400

    Fix the infinite loop error

--- GreatestCommonDivisor.java
+++ GreatestCommonDivisor.java
@@ -9,18 +9,20 @@ public class GreatestCommonDivisor {
 		Scanner input = new Scanner(System.in);
 		
 		// Prompt the user to enter two integers
-		System.out.print("Enter first integer: ");
+		System.out.print("1 Enter first integer: ");
 		int n1 = input.nextInt();
-		System.out.print("Enter second integer: ");
-		int n2 = innput.nextInt();
+		System.out.print("2 Enter second integer: ");
+		int n2 = input.nextInt();
 		
 		int gcd = 1;
 		int k = 2;
-		while (k <= n1 && k <= n2)
+		while (k <= n1 && k <= n2) {
 			if (n1 % k == 0 && n2 % k == 0) {
 				gcd = k;
-				k++;
 			}
+			k++;
+			
+		}
 		
 		System.out.println("The greatest common divisor for " + n1 + 
 				" and " + n2 + " is " + gcd);

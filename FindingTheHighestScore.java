b3802d0ea367a2f0d89206ba7d123b17493b8698
ygchan89 <ygchan89@gmail.com>
2013-07-04 21:48:41 -0400

    Add the pseudocode

---
+++ FindingTheHighestScore.java
@@ -0,0 +1,18 @@
+// A program that prompts the user to enter the number of students and each
+// student's name and score, display the student with the highest score and
+// and student with second highest score
+
+public class FindingTheHighestScore{
+	public static void main(String arg[] ){
+		// Create a Scanner
+		// Prompt the user to ask how many students are there
+		
+		// For (n) student..
+			// Ask for name
+			// Ask for score
+			// Test if this student is the highest?
+				// If so, update
+		
+		// Display the result
+	}
+}

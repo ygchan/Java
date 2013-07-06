70b8c6dfc2400de7ba6cf6493aa51eb2ad3f0e03
ygchan89 <ygchan89@gmail.com>
2013-07-06 16:40:23 -0400

    Add the pseudocode

---
+++ DisplayPyramid.java
@@ -0,0 +1,20 @@
+// A program that prompts the user to enter an integer from 1 to 15
+// and display a pyramid.
+
+public class DisplayPyramid {
+	public static void main(String[] args) {
+		// Create a Scanner
+		
+		// While 1 <= numberOfLines <= 15
+			// Prompt user for input
+		
+		// Display a pyramid
+		// -----------------
+		// For i = 0, i <= numberOfLines, i++ ...
+		// numberOfSpaces = numberOfLines * 2 
+			// For j = 0, j <= nextOddNumber, j++
+			// Print i 
+			// numberOfSpace = numberOfSpace - 2
+		
+	}
+}

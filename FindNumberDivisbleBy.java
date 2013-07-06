7b1d8d9795d62bf650ce0926a845cf6b06c8441f
ygchan89 <ygchan89@gmail.com>
2013-07-06 10:35:44 -0400

    Add a file

---
+++ FindNumberDivisbleBy.java
@@ -0,0 +1,8 @@
+// A program that displays all the numbers from 100 to 1000,
+// ten per line, that are divisible by 5 and 6.
+
+public class FindNumberDivisbleBy {
+	public static void main(String[] args) {
+		
+	}
+}

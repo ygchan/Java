ba003bcf18c8c112c512ce3367f6acf5b606fd70
ygchan89 <ygchan89@gmail.com>
2013-07-06 11:28:55 -0400

    Test int to ACSII char

---
+++ DisplayACSIITable.java
@@ -0,0 +1,11 @@
+// Display the characters in the ACSII character table from '!' to '~'
+
+public class DisplayACSIITable{
+	public static void main(String[] args) {
+		// Test if integer 33 equal to '!' in ASCII
+		int number = 33;
+		char character = (char)number;
+		
+		System.out.println(character);
+	}
+}

5836fe8c11b8b22929cca25d7b19e9e63c8d641a
ygchan89 <ygchan89@gmail.com>
2013-07-06 10:39:35 -0400

    Build the display 10 number / line

--- FindNumberDivisbleBy.java
+++ FindNumberDivisbleBy.java
@@ -3,6 +3,16 @@
 
 public class FindNumberDivisbleBy {
 	public static void main(String[] args) {
-		
+		// Display 10 numbers per line, to display 2 of these lines
+		final int NUMBER_PER_LINE = 10;
+		for (int i = 1; i <= 20; i++) {
+			// Go to next line when it is 10th number, or the 1st
+			if (i % NUMBER_PER_LINE == 0) 
+				System.out.println(i + " ");
+			
+			// Otherwise append to it on the same line
+			else
+				System.out.print(i + " ");
+		}
 	}
 }

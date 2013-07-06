2b4cb77e3bb51258c1c87d20623e2da0faf1d8ba
ygchan89 <ygchan89@gmail.com>
2013-07-06 11:46:08 -0400

    Add the pseudocode

---
+++ GreatestCommonDivisor2.java
@@ -0,0 +1,17 @@
+// Another method to compute the greatest common divisor
+// Start from the smaller of n1 and n2. Then we check if d, d-1, d-2 ...
+// is the divisor for both n1 and n2.
+
+public class GreatestCommonDivisor2 {
+	public static void main(String[] args) {
+		// Find the smaller between n1 and n2
+		
+		// Determine if d is the common divisor for both n1 and n2
+		
+		// If so, break
+		
+		// Otherwise, decrement d
+		
+		// Print the result
+	}
+}

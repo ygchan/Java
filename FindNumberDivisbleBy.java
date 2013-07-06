f42e28bde7cd2738c948cb8a3b412f31da42123a
ygchan89 <ygchan89@gmail.com>
2013-07-06 10:50:38 -0400

    Practice XP agile method

--- FindNumberDivisbleBy.java
+++ FindNumberDivisbleBy.java
@@ -5,25 +5,23 @@ public class FindNumberDivisbleBy {
 	public static void main(String[] args) {
 		// Display 10 numbers per line, to display 2 of these lines
 		final int NUMBER_PER_LINE = 10;
-		
-		for (int i = 1; i <= 20; i++) {
-			// Go to next line when it is 10th number, or the 1st
-			if (i % NUMBER_PER_LINE == 0) 
-				System.out.println(i + " ");
+		int counter = 1;
+
+		for (int number = 100; number <= 1000; number = number + 1) {
+			// True if it is divisible by 5 and 6
+			boolean isDivisible =
+					((number % 5 == 0) && (number % 6 == 0));
 			
-			// Otherwise append to it on the same line
-			else
-				System.out.print(i + " ");
+			if (isDivisible) {
+				// Go to next line when it is 10th number, or the 1st
+				if (counter % NUMBER_PER_LINE == 0) 
+					System.out.println(number + " ");
+				
+				// Otherwise append to it on the same line
+				else
+					System.out.print(number + " ");
+				counter++;
+			}
 		}
-		
-		int testNumber = 30; // Test case we know is divisible by 5 and 6
-		int testNumber2 = 25; // Test case we know it does not
-		
-		// Find out if a number is divisible by 5 and 6
-		if ((testNumber2 % 5 == 0) && (testNumber2 % 6 == 0))
-			System.out.println(testNumber2 + " is divisible by 5 and 6.");
-		else 
-			System.out.println(testNumber2 + " is not divisible both by 5 and 6.");
-		
 	}
 }

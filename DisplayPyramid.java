3fb03d02a48362eb031b06897d7eccca3fd08a34
ygchan89 <ygchan89@gmail.com>
2013-07-07 17:38:53 -0400

    Work on the problem

--- DisplayPyramid.java
+++ DisplayPyramid.java
@@ -1,20 +1,36 @@
 // A program that prompts the user to enter an integer from 1 to 15
 // and display a pyramid.
 
+import java.util.Scanner;
+
 public class DisplayPyramid {
 	public static void main(String[] args) {
 		// Create a Scanner
+		Scanner input = new Scanner(System.in);
+		int numberOfLines = 0; // Hold the user input
 		
-		// While 1 <= numberOfLines <= 15
+		// While !(1 <= numberOfLines <= 15)
+		do {
 			// Prompt user for input
+			System.out.print("Enter the number of lines: ");
+			numberOfLines = input.nextInt();
+		} while (!(1 <= numberOfLines) && !(numberOfLines <= 15));
 		
-		// Display a pyramid
-		// -----------------
-		// For i = 0, i <= numberOfLines, i++ ...
-		// numberOfSpaces = numberOfLines * 2 
-			// For j = 0, j <= nextOddNumber, j++
-			// Print i 
-			// numberOfSpace = numberOfSpace - 2
+		int numberOfSpaces = 0;
 		
+		// How many space in front of the first character
+		if (numberOfLines != 1) 
+			 numberOfSpaces = (numberOfLines - 1 ) * 2;
+	
+		
+		
+		for (int i = 1; i <= numberOfLines; i++) {
+			// Display the space offset of the first character
+			for (int j = 0; j <= numberOfSpaces; j++) 
+				System.out.print(" ");
+				System.out.print("Hello World");
+			for (int k = i; k <= i; k++)
+				System.out.print(k);
+		}
 	}
 }

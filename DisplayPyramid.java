88f33bc93e2c48bb9c11c46bf8a1e62de662b4f1
ygchan89 <ygchan89@gmail.com>
2013-07-07 19:06:14 -0400

    Add sample output

--- DisplayPyramid.java
+++ DisplayPyramid.java
@@ -1,5 +1,17 @@
 // A program that prompts the user to enter an integer from 1 to 15
 // and display a pyramid.
+/*
+                1 
+              2 1 2 
+            3 2 1 2 3 
+          4 3 2 1 2 3 4 
+        5 4 3 2 1 2 3 4 5 
+      6 5 4 3 2 1 2 3 4 5 6 
+    7 6 5 4 3 2 1 2 3 4 5 6 7 
+  8 7 6 5 4 3 2 1 2 3 4 5 6 7 8 
+9 8 7 6 5 4 3 2 1 2 3 4 5 6 7 8 9 
+
+*/
 
 import java.util.Scanner;
 

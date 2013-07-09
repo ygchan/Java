8ec0246eb763a77bbeecd127fa41aa690b9302d6
ygchan89 <ygchan89@gmail.com>
2013-07-08 23:19:50 -0400

    Test double as the control variable

--- ComputeLoansWithVariousInterest.java
+++ ComputeLoansWithVariousInterest.java
@@ -2,13 +2,25 @@
 // number of years and display the monthly and total payment for each 
 // interest rate starting from 5% to 8%, with an increment of 1/8
 
+import java.util.Scanner;
+
 public class ComputeLoansWithVariousInterest {
 	public static void main(String[] args) {
 		// Create a Scanner
+		Scanner input = new Scanner(System.in);
 		
 		// Prompt user for years
+		System.out.print("Loan Amount: ");
+		double loanAmount = input.nextDouble();
+		
+		System.out.print("Number of Years: ");
+		int numberOfYear = input.nextInt();
 		
 		// From 5% to 8%...
+		for (double interestRate = 5.0; interestRate <= 8.0; 
+				interestRate = interestRate + 0.125) {
+			System.out.println(interestRate);
+		}
 			// Compute the monthly payment and total payment
 			// Monthly payment: 
 			// Total payment:

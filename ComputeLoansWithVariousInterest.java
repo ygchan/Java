aa23f8d9be1c39a0fbfe7ec2395f8e580e9cb38d
ygchan89 <ygchan89@gmail.com>
2013-07-08 23:13:54 -0400

    Add a file

---
+++ ComputeLoansWithVariousInterest.java
@@ -0,0 +1,16 @@
+// A program that lets the user enter the loan amount and loan period in
+// number of years and display the monthly and total payment for each 
+// interest rate starting from 5% to 8%, with an increment of 1/8
+
+public class ComputeLoansWithVariousInterest {
+	public static void main(String[] args) {
+		// Create a Scanner
+		
+		// Prompt user for years
+		
+		// From 5% to 8%...
+			// Compute the monthly payment and total payment
+			// Monthly payment: 
+			// Total payment:
+	}
+}

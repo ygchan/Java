abf4206c6c999181f7e53eb86e6639f328425b7e
ygchan89 <ygchan89@gmail.com>
2013-07-09 22:38:31 -0400

    Add the implementation

---
+++ ComputeLoanWithAmortization.java
@@ -0,0 +1,20 @@
+public class ComputeLoanWithAmortization {
+	public static void main(String[] args) {
+		// Create a Scanner
+		
+		// Prompt for years and annual interest rate
+		
+		// Compute monthly interest rate
+		// Monthly interest rate: Annual Interest / 12 / 100
+		// The balance: The remaining principal
+		
+		// Compute monthly payment
+		// Monthly payment: loan amount * monthly interest rate / 
+		//                  (1 - 1 / (1 + monthly interest rate) ^ years * 12
+		
+		// Compute total payment
+		// Total payment = monthly payment * 12 * years
+		
+		// Display the payment plan
+	}
+}

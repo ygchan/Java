76eaad000a75c191c56b791c09b4ca33971b2252
ygchan89 <ygchan89@gmail.com>
2013-07-12 00:04:14 -0400

    Print 10 times

--- HelloJava.java
+++ HelloJava.java
@@ -1,8 +1,8 @@
-// Print Hello Java 5 times
+// Print Hello Java 10 times
 
 public class HelloJava {
 	public static void main(String[] args) {
-		int count = 5;
+		int count = 10;
 		
 		while (count > 0) {
 			System.out.println("Hello Java");
